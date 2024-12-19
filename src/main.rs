50497245abf32aea30fe6a72c29551e9fd69c14d
Stefan Simon <stefan.j.c.simon@gmail.com>
2024-12-19 23:06:33 +0100

    [TEST] Add test

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ pub struct AnyOther {
     name: String,
 }
 
-
 // after transformation
 // pub fn do_stuff(session: Session) {
 //     let token: Box<dyn AuthToken> = Box::new(SessionAuthToken::new(session));
-    
+
 //     if token.is_authenticated() {
 //         println!("Yes, you are authenticated !!!");
 //     } else {
@@ -19,20 +18,34 @@ pub struct AnyOther {
 
 // input
 #[authenticated]
-pub fn before_do_stuff(a_token: Box<dyn AuthToken>, any: AnyOther) {
+pub fn before_do_stuff(a_token: Box<dyn AuthToken>, any: AnyOther) -> bool {
     if a_token.is_authenticated() {
         println!("Yes, you are authenticated !!! Any = {}", any.name);
-    } else {
-        println!("Oh no, you are not authenticated :(");
+        return true;
     }
+
+    println!("Oh no, you are not authenticated :(");
+
+    false
 }
 
 fn main() {
-    let s = Session::new("admin");
-    let a = AnyOther { name: "any other stuff".to_string() };
-
-    before_do_stuff(s, a);
     println!("Running");
 }
 
+#[cfg(test)]
+mod test {
+    use types::Session;
 
+    use crate::{before_do_stuff, AnyOther};
+
+    #[test]
+    fn should_mutate_function() {
+        let s = Session::new("admin");
+        let a = AnyOther {
+            name: "any other stuff".to_string(),
+        };
+
+        assert!(before_do_stuff(s, a));
+    }
+}
