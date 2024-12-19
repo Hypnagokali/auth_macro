5fc5d4d789e4bdf80e40c098b0a3710c6df684de
Stefan Simon <stefan.j.c.simon@gmail.com>
2024-12-19 23:03:08 +0100

    [*] Visibility of fn and refactoring

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,28 +1,19 @@
-use std::any::Any;
-
-use authenticated::auth_function::{simple_type_check_is_box_dyn_auth_token, AuthFunction};
+use authenticated::auth_function::AuthFunction;
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
-use syn::{parse_macro_input, punctuated::Punctuated, token::Comma, FnArg, ItemFn, Stmt};
+use quote::ToTokens;
+use syn::{parse_macro_input,ItemFn};
 
 mod authenticated;
 
 #[proc_macro_attribute]
 pub fn authenticated(_: TokenStream, input: TokenStream) -> TokenStream {
     println!("Do transform 'authenticate'");
-    let mut ast = parse_macro_input!(input as ItemFn);
-
+    let ast = parse_macro_input!(input as ItemFn);
 
     match AuthFunction::new(ast.clone()) {
         Ok(func) => func.into_token_stream().into(),
         Err(e) => TokenStream::from(e.to_compile_error()),
     }
-
-    // println!("{}", t.to_string());
-
-    // quote! {
-    //     #ast
-    // }.into()
 }
 
 #[proc_macro_attribute]
